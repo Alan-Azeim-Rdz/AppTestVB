c3363a413a727d92b742b7bfaae1cf8b155fc3f7
Alan-Azeim-Rdz <i23050359@monclova.tecnm.mx>
2024-05-01 13:55:49 -0600

    Agregar archivos de proyecto.

---
+++ AppTestVB/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnSave = New Button()
+        btnRemove = New Button()
+        Label1 = New Label()
+        txtDescription = New TextBox()
+        lstvData = New ListView()
+        ColumnHeader1 = New ColumnHeader()
+        ColumnHeader2 = New ColumnHeader()
+        ColumnHeader3 = New ColumnHeader()
+        lstItems = New ListBox()
+        CreteFileBtn = New Button()
+        lblCount = New Label()
+        LblTotal = New Label()
+        SuspendLayout()
+        ' 
+        ' btnSave
+        ' 
+        btnSave.Font = New Font("Segoe UI", 14F)
+        btnSave.Location = New Point(452, 49)
+        btnSave.Name = "btnSave"
+        btnSave.Size = New Size(130, 47)
+        btnSave.TabIndex = 0
+        btnSave.Text = "Save"
+        btnSave.UseVisualStyleBackColor = True
+        ' 
+        ' btnRemove
+        ' 
+        btnRemove.Font = New Font("Segoe UI", 14F)
+        btnRemove.Location = New Point(603, 49)
+        btnRemove.Name = "btnRemove"
+        btnRemove.Size = New Size(145, 47)
+        btnRemove.TabIndex = 1
+        btnRemove.Text = "Remove"
+        btnRemove.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 15F)
+        Label1.Location = New Point(48, 48)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(112, 28)
+        Label1.TabIndex = 2
+        Label1.Text = "Description"
+        ' 
+        ' txtDescription
+        ' 
+        txtDescription.Location = New Point(166, 48)
+        txtDescription.Multiline = True
+        txtDescription.Name = "txtDescription"
+        txtDescription.Size = New Size(179, 34)
+        txtDescription.TabIndex = 1
+        ' 
+        ' lstvData
+        ' 
+        lstvData.Columns.AddRange(New ColumnHeader() {ColumnHeader1, ColumnHeader2, ColumnHeader3})
+        lstvData.Font = New Font("Segoe UI", 15F)
+        lstvData.Location = New Point(321, 102)
+        lstvData.Name = "lstvData"
+        lstvData.Size = New Size(467, 261)
+        lstvData.TabIndex = 4
+        lstvData.UseCompatibleStateImageBehavior = False
+        lstvData.View = View.Details
+        ' 
+        ' ColumnHeader1
+        ' 
+        ColumnHeader1.Text = "ID"
+        ColumnHeader1.Width = 120
+        ' 
+        ' ColumnHeader2
+        ' 
+        ColumnHeader2.Text = "Description"
+        ColumnHeader2.Width = 120
+        ' 
+        ' ColumnHeader3
+        ' 
+        ColumnHeader3.Text = "PIRCE"
+        ColumnHeader3.Width = 120
+        ' 
+        ' lstItems
+        ' 
+        lstItems.FormattingEnabled = True
+        lstItems.ItemHeight = 15
+        lstItems.Location = New Point(12, 102)
+        lstItems.Name = "lstItems"
+        lstItems.Size = New Size(260, 244)
+        lstItems.TabIndex = 5
+        ' 
+        ' CreteFileBtn
+        ' 
+        CreteFileBtn.Font = New Font("Segoe UI", 14F)
+        CreteFileBtn.Location = New Point(48, 380)
+        CreteFileBtn.Name = "CreteFileBtn"
+        CreteFileBtn.Size = New Size(157, 47)
+        CreteFileBtn.TabIndex = 6
+        CreteFileBtn.Text = "Create File Exel"
+        CreteFileBtn.UseVisualStyleBackColor = True
+        ' 
+        ' lblCount
+        ' 
+        lblCount.AutoSize = True
+        lblCount.Font = New Font("Segoe UI", 13F)
+        lblCount.Location = New Point(378, 396)
+        lblCount.Name = "lblCount"
+        lblCount.Size = New Size(0, 25)
+        lblCount.TabIndex = 7
+        ' 
+        ' LblTotal
+        ' 
+        LblTotal.AutoSize = True
+        LblTotal.Font = New Font("Segoe UI", 13F)
+        LblTotal.Location = New Point(685, 393)
+        LblTotal.Name = "LblTotal"
+        LblTotal.Size = New Size(0, 25)
+        LblTotal.TabIndex = 8
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(LblTotal)
+        Controls.Add(lblCount)
+        Controls.Add(CreteFileBtn)
+        Controls.Add(lstItems)
+        Controls.Add(lstvData)
+        Controls.Add(txtDescription)
+        Controls.Add(Label1)
+        Controls.Add(btnRemove)
+        Controls.Add(btnSave)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtDescription As TextBox
+    Friend WithEvents lstvData As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents lstItems As ListBox
+    Friend WithEvents CreteFileBtn As Button
+    Friend WithEvents lblCount As Label
+    Friend WithEvents LblTotal As Label
+
+End Class
---
+++ AppTestVB/Form1.vb
@@ -0,0 +1,119 @@
+﻿Imports System.Drawing.Text
+Imports System.Security.Cryptography
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports DocumentFormat.OpenXml
+Imports DocumentFormat.OpenXml.Office2010.Drawing
+Imports DocumentFormat.OpenXml.Packaging
+Imports DocumentFormat.OpenXml.Spreadsheet
+Imports System
+
+Public Class Form1
+
+    Dim id As Integer
+
+    Public Sub New()
+        InitializeComponent()
+        id = 0
+        lstvData.FullRowSelect = True
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim x As MyItem
+        'i++   -->   i = i + 1
+        Dim description As String = txtDescription.Text
+        id = id + 1
+        Dim Price As Random = New Random()
+
+        x = New MyItem(id, description, Math.Round(Price.NextDouble() * 1000, 2))
+        lstItems.Items.Add(x.ToString())
+
+        'ListView -- ListViewItems -- SubItems
+
+        For i = 1 To 100
+            Dim row As ListViewItem = New ListViewItem(x.Id)
+            row.SubItems.Add(x.Description)
+            row.SubItems.Add(x.Price)
+            lstvData.Items.Add(row)
+            x.Id = x.Id + 1
+            x.Price = Math.Round(Price.NextDouble() * 1000, 2)
+
+        Next
+        UpdateLabel()
+        UpdateTotal()
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        If lstvData.SelectedItems.Count = 0 Then
+            Return
+        End If
+        For Each item As ListViewItem In lstvData.SelectedItems
+            lstvData.Items.Remove(item)
+        Next
+        UpdateLabel()
+        UpdateTotal()
+    End Sub
+
+    Sub UpdateLabel()
+        lblCount.Text = lstvData.Items.Count
+    End Sub
+    Sub UpdateTotal()
+        Dim Total As Decimal = 0
+        For Each item As ListViewItem In lstvData.Items
+            Total = Total + Decimal.Parse(item.SubItems(2).Text)
+        Next
+        LblTotal.Text = Total
+    End Sub
+
+    Private Sub CreteFileBtn_Click(sender As Object, e As EventArgs) Handles CreteFileBtn.Click
+        Dim savefile As SaveFileDialog = New SaveFileDialog()
+        savefile.Filter = "Exel Files|*.xlsx"
+        savefile.Title = "Save the Text File in Exel"
+
+        If savefile.ShowDialog() = DialogResult.OK Then
+            Dim Namefile As String = savefile.FileName
+
+            Using DocumentExel As SpreadsheetDocument = SpreadsheetDocument.Create(Namefile, SpreadsheetDocumentType.Workbook)
+
+                Dim workbook As WorkbookPart = DocumentExel.AddWorkbookPart()
+                workbook.Workbook = New Workbook()
+
+                ' Agregar la hoja de cálculo
+                Dim worksheetPart As WorksheetPart = workbook.AddNewPart(Of WorksheetPart)()
+                worksheetPart.Worksheet = New Worksheet(New SheetData())
+
+                ' Añadir la hoja de cálculo al workbook
+                Dim sheets As Sheets = workbook.Workbook.AppendChild(New Sheets())
+                Dim sheet As New Sheet With {
+                    .id = workbook.GetIdOfPart(worksheetPart),
+                    .SheetId = 1,
+                    .Name = "Hoja1"
+                 }
+                sheets.Append(sheet)
+
+                ' Obtener los datos de la ListView y escribirlos en el archivo Excel
+                Dim sheetData As SheetData = worksheetPart.Worksheet.GetFirstChild(Of SheetData)()
+                For Each item As ListViewItem In lstvData.Items
+                    Dim row As New Row()
+                    For Each subItem As ListViewItem.ListViewSubItem In item.SubItems
+                        Dim cell As New Cell With {
+                        .DataType = CellValues.String,
+                        .CellValue = New CellValue(subItem.Text)
+                    }
+                        row.Append(cell)
+                    Next
+                    sheetData.Append(row)
+                Next
+
+                ' Guardar el documento
+                workbook.Workbook.Save()
+
+            End Using
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
